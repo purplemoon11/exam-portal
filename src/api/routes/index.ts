b8d246a0d01f276f1249e86383a3973b95d23292
Abishkar Karki <abishkarkarki@Abishkars-MacBook-Air.local>
2023-10-26 16:40:32 +0545

    feat: created forgot password

--- src/api/routes/index.ts
+++ src/api/routes/index.ts
@@ -1,5 +1,8 @@
 import express from "express";
+import Authentication from "./authentication/authentication.route";
 
 const router = express.Router();
+
+router.use("/authentication", Authentication);
 
 export default router;
--- src/config/express.ts
+++ src/config/express.ts
@@ -1,7 +1,9 @@
-import express from "express"
-import cors from "cors"
+import express from "express";
+import cors from "cors";
+import session from "express-session";
+import V1Route from "../api/routes/index";
 
-const app = express()
+const app = express();
 
 app.use(
   cors({
@@ -10,9 +12,24 @@ app.use(
     preflightContinue: false,
     optionsSuccessStatus: 200,
   })
-)
+);
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.set("trust proxy", 1);
+app.use(
+  session({
+    name: "pdot",
+    secret: String(process.env.SESSION_SECRET),
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false,
+      maxAge: 60000,
+    },
+  })
+);
 
-export default app
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api/v1", V1Route);
+
+export default app;
