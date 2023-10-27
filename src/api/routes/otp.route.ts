7ada499883e878be33de425c1f6955c761fc7249
AnupLamsal <anuplamsal7410@gmail.com>
2023-10-27 11:40:46 +0545

    Remove test otp

--- src/api/routes/otp.route.ts
+++ src/api/routes/otp.route.ts
@@ -1,11 +1,10 @@
 import { Router } from "express";
-import { createOtp, verifyOtp, testOtp } from "../controllers/otp.controller";
+import { createOtp, verifyOtp } from "../controllers/otp.controller";
 import { authUser } from "../middlewares/auth.middleware";
 
 const router = Router();
 
 router.route("/send").post(authUser, createOtp);
 router.route("/verify").get(authUser, verifyOtp);
-router.route("/").get(authUser, testOtp);
 
 export default router;
