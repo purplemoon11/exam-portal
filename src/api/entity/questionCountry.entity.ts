430b3eac448d24fb61b2f3a0876bdab5efd8c502
AnupLamsal <anuplamsal7410@gmail.com>
2023-10-30 20:57:44 +0545

    Country get by id data done

---
+++ src/api/entity/questionCountry.entity.ts
@@ -0,0 +1,24 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm"
+import { ExamQuestion } from "./question.entity"
+
+@Entity("exam_question_country", { schema: "public" }) // Specify the schema if needed
+export class ExamQuestionCountry {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @Column({ name: "country_name", type: "varchar" })
+  country_name: string
+
+  @Column({ name: "question_id", type: "integer" })
+  question_id: number
+
+  @ManyToOne(() => ExamQuestion)
+  @JoinColumn({ name: "question_id" })
+  question: ExamQuestion
+}
