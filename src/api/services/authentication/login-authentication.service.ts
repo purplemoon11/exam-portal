a2e73ec920692583a92524fa3a7970f7ea8430a2
Abishkar Karki <abishkarkarki@Abishkars-MacBook-Air.local>
2023-10-30 10:16:19 +0545

    bug fixing

--- src/api/services/authentication/login-authentication.service.ts
+++ src/api/services/authentication/login-authentication.service.ts
@@ -9,31 +9,48 @@ const userRepository = datasource.getRepository(User); // Get the repository fro
 
 export const loginUser = async (data: LoginData) => {
   const { passportNum, password } = data;
-  const user = await userRepository.findOneBy({ passportNum });
-  if (!user) {
-    throw new AppErrorUtil(
-      404,
-      "The email or password you entered is incorrect"
-    );
+
+  if (!passportNum || !password) {
+    throw new AppErrorUtil(400, "Passport number and password are required");
   }
-  const hash = user.password;
-  const verify = await bcrypt.compare(password, hash);
-  if (!verify) {
-    throw new AppErrorUtil(400, "The email or password you entered is invalid");
-  } else {
-    const payload = {
-      id: user.id,
-      email: user.email,
-      full_name: user.fullname,
-      passport: user.passportNum,
-      phNumber: user.phNumber,
-    };
-    const secretKey = process.env.JWT_SECRET_KEY
-      ? process.env.JWT_SECRET_KEY
-      : "";
-    const token = jwt.sign(payload, secretKey);
-    const { password, ...userData } = user;
-    console.log(verify, token, userData);
-    return { verify, token, data: userData };
+
+  try {
+    const user = await userRepository.findOneBy({ passportNum });
+
+    if (!user) {
+      throw new AppErrorUtil(
+        404,
+        "The email or password you entered is incorrect"
+      );
+    }
+
+    const hash = user.password;
+
+    const verify = await bcrypt.compare(password, hash);
+
+    if (!verify) {
+      throw new AppErrorUtil(
+        400,
+        "The email or password you entered is invalid"
+      );
+    } else {
+      const payload = {
+        id: user.id,
+        email: user.email,
+        full_name: user.fullname,
+        passport: user.passportNum,
+        phNumber: user.phNumber,
+      };
+
+      const secretKey = process.env.JWT_SECRET_KEY || "";
+      const token = jwt.sign(payload, secretKey);
+
+      const { password, ...userData } = user;
+
+      console.log(verify, token, userData);
+      return { verify, token, data: userData };
+    }
+  } catch (error) {
+    throw new AppErrorUtil(500, "Internal Server Error");
   }
 };
