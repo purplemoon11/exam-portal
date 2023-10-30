3ab20e3914ea168d878095d21ab1e910041a794d
AnupLamsal <anuplamsal7410@gmail.com>
2023-10-30 11:31:15 +0545

    Solved issue of user update password

--- src/api/services/authentication/set-password.service.ts
+++ src/api/services/authentication/set-password.service.ts
@@ -3,7 +3,7 @@ import ormConfig from "../../../config/ormConfig"
 
 const userRepository = ormConfig.getRepository(User)
 
-export const userUpdatePass = async (password: string, user: object) => {
+export const userUpdatePass = async (password: string, user: User) => {
   const pass = userRepository.merge(user, { password })
 
   return pass
