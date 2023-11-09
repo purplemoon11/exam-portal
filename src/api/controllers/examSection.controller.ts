0de7594ff1232a61a4007c025731368edfd0990b
AnupLamsal <anuplamsal7410@gmail.com>
2023-11-09 16:53:17 +0545

    exam section done with country id

--- src/api/controllers/examSection.controller.ts
+++ src/api/controllers/examSection.controller.ts
@@ -1,11 +1,13 @@
 import { Request, Response, NextFunction } from "express"
 import { ExamSection } from "../entity/examSection.entity"
 import { Cluster } from "../entity/admin/Master-Data/cluster.entity"
+import { Country } from "../entity/country.entity"
 import ormConfig from "../../config/ormConfig"
 import logger from "../../config/logger"
 
 const examSectionRepo = ormConfig.getRepository(ExamSection)
 const clusterRepo = ormConfig.getRepository(Cluster)
+const countryRepo = ormConfig.getRepository(Country)
 
 export const createExamSection = async (
   req: Request,
@@ -13,7 +15,7 @@ export const createExamSection = async (
   next: NextFunction
 ) => {
   try {
-    const { name, cluster_id, noOfQue } = req.body
+    const { name, cluster_id, country_id, noOfQue } = req.body
 
     const cluster = await clusterRepo.findOne({ where: { id: cluster_id } })
 
@@ -21,10 +23,17 @@ export const createExamSection = async (
       return res.status(400).json({ message: "Cluster not found" })
     }
 
+    const country = await countryRepo.findOne({ where: { id: country_id } })
+
+    if (!country) {
+      return res.status(400).json({ message: "Country not found" })
+    }
+
     const examSectionData = new ExamSection()
 
     examSectionData.name = name
     examSectionData.cluster_id = cluster_id
+    examSectionData.country_id = country_id
     examSectionData.noOfQuestions = noOfQue
 
     const examSection = await examSectionRepo.save(examSectionData)
@@ -45,6 +54,26 @@ export const getExamSectionById = async (
   try {
     const id = parseInt(req.params.id)
     const examSection = await examSectionRepo.findOne({ where: { id } })
+
+    if (!examSection) {
+      return res.status(400).json({ message: "Exam section not found" })
+    }
+
+    res.json({ data: examSection })
+  } catch (err) {
+    logger.error(err)
+    res.status(500).send(err)
+  }
+}
+
+export const getExamSectionByCountryId = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const country_id = parseInt(req.params.country_id)
+    const examSection = await examSectionRepo.find({ where: { country_id } })
 
     if (!examSection) {
       return res.status(400).json({ message: "Exam section not found" })
@@ -79,7 +108,7 @@ export const updateExamSection = async (
 ) => {
   try {
     const id = parseInt(req.params.id)
-    const { name, cluster_id, noOfQue } = req.body
+    const { name, cluster_id, country_id, noOfQue } = req.body
 
     let examSection = await examSectionRepo.findOne({ where: { id } })
 
@@ -93,7 +122,13 @@ export const updateExamSection = async (
       return res.status(400).json({ message: "Cluster not found" })
     }
 
-    const examSectionData = { name, cluster_id, noOfQue }
+    const country = await countryRepo.findOne({ where: { id: country_id } })
+
+    if (!country) {
+      return res.status(400).json({ message: "Country not found" })
+    }
+
+    const examSectionData = { name, cluster_id, country_id, noOfQue }
 
     examSection = examSectionRepo.merge(examSection, examSectionData)
 
--- src/api/entity/examSection.entity.ts
+++ src/api/entity/examSection.entity.ts
@@ -6,6 +6,7 @@ import {
   JoinColumn,
 } from "typeorm"
 import { Cluster } from "./admin/Master-Data/cluster.entity"
+import { Country } from "./country.entity"
 
 @Entity("exam_section")
 export class ExamSection {
@@ -21,7 +22,14 @@ export class ExamSection {
   @Column({ name: "no_questions", type: "int" })
   noOfQuestions: number
 
+  @Column({ name: "country_id", type: "int" })
+  country_id: number
+
   @ManyToOne(() => Cluster)
   @JoinColumn({ name: "cluster_id" })
   clusterId: Cluster[]
+
+  @ManyToOne(() => Country)
+  @JoinColumn({ name: "country_id" })
+  countryId: Country[]
 }
