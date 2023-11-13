e517b4f9f67f1e9cc7e9189a17403c69c84c3605
AnupLamsal <anuplamsal7410@gmail.com>
2023-11-13 13:03:17 +0545

    pull branch main

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -1,33 +1,33 @@
-import { Redis } from "ioredis";
+import { Redis } from "ioredis"
 
 export const redisClient = new Redis({
   host: "localhost",
   port: Number(process.env.REDIS_PORT),
-});
+})
 
 export const getAsync = (key: any) => {
   return new Promise((resolve, reject) => {
     redisClient.get(key, (err, data) => {
-      if (err) reject(err);
-      resolve(data);
-    });
-  });
-};
+      if (err) reject(err)
+      resolve(data)
+    })
+  })
+}
 
 export const setAsync = (key: any, value: any): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
-    redisClient.set(key, value, (err) => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
-};
+    redisClient.set(key, value, err => {
+      if (err) reject(err)
+      resolve()
+    })
+  })
+}
 
 export const delAsync = (key: any): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
-    redisClient.del(key, (err) => {
-      if (err) reject(err);
-      resolve();
-    });
-  });
-};
+    redisClient.del(key, err => {
+      if (err) reject(err)
+      resolve()
+    })
+  })
+}
